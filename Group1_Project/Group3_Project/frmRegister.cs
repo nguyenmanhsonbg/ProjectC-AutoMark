133cca5d8c76ea3552d6b605d4d39a710e5c6ca8
Your Name <you@example.com>
2021-08-11 18:19:00 +0700

    first commit

---
+++ Group1_Project/Group3_Project/frmRegister.cs
@@ -0,0 +1,57 @@
+﻿using Group1_Project.DAO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Group3_Project
+{
+    public partial class frmRegister : Form
+    {
+        public frmRegister()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Equals("")) MessageBox.Show("Username cannot blank!");
+            else if (textBox2.Text.Equals("")) MessageBox.Show("Password cannot blank!");
+            else if(textBox3.Text.Equals("")) MessageBox.Show("Please confirm password!");
+            else if (textBox2.Text != textBox3.Text)
+            {
+                label7.Visible = true;
+                label7.ForeColor = Color.Red;
+                label7.Text = "Password and Confirm Password doesn't match!";
+            }
+            else
+            {
+                if (DAO.checkAccountExist(textBox1.Text) == true)
+                {
+                    label7.Visible = true;
+                    label7.ForeColor = Color.Red;
+                    label7.Text = "Username already exist!Please choose another name!";
+                }
+                else
+                {
+                    label7.Visible = false;
+                    DAO.insertAccount(textBox1.Text, textBox2.Text);
+                    label7.Visible = true;
+                    label7.ForeColor = Color.Green;
+                    label7.Text = "Create account successfully";
+                }
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            new frmLogin().ShowDialog();
+        }
+    }
+}
